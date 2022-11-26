9e53ce6678b140ed372deadbe5f29dd86912861d
RohitMangale <rohitgitlab@gmail.com>
2022-11-26 15:20:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/languagetranslator/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.example.languagetranslator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        lottie = findViewById(R.id.Lottie);
+
+        lottie.animate().setDuration(6000).setStartDelay(0);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },6000);
+    }
+}
